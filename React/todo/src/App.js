47eddc32025015238a6def6a3b35e3b52c9dc534
Tony Diethelm <tonydiethelm@gmail.com>
2023-02-28 13:32:59 -0800

    Adding Angular

--- React/todo/src/App.js
+++ React/todo/src/App.js
@@ -5,18 +5,35 @@ function App() {
   //set up state to track my input and my todolist. 
   const [input, setInput] = useState("");
   const [todoList, setTodoList] = useState([]);
+  const [catFact, setCatFact] = useState("");
 
   //handleclick functions.
+  //get todo, put in todo list in state
+  //clear the input box
+  //update cat facts in state
   const handleClick = () => {
     console.log('button was clicked.')
+    //update todo list
     setTodoList((prev) => [
       ...prev,
       {
         task: input,
       },
     ]);
-    setInput(""); //changes state, so waterfalls and resets box. Cheap and easy way. 
+    //reset text box to ""
+    setInput("");
+    //update state with new cat fact
+    setCatFact(() => {getCatFact()});
   };
+
+  //function to get cat facts
+  async function getCatFact(){
+    console.log('attempting to get cat fact');
+    const response = await fetch('https://catfact.ninja/fact')
+    const catFact = await response.json();
+    console.log('cat fact is: ', catFact.fact);
+    return catFact.fact;
+  }
 
   //My UI elements
   return (
@@ -36,7 +53,10 @@ function App() {
         <input type="button" onClick={() => handleClick()} value="Press to add Todo to list"/>
       </form>
 
-      
+      <p>Here is a random cat fact: {catFact}</p>
+
+
+
     </div>
   );
 }
