ae385629e401a6dde894d14036e57d0bfccc487c
Tony Diethelm <tonydiethelm@gmail.com>
2023-05-31 14:23:19 -0700

    Oops, adding Svelte, and some angular files.

---
+++ Angular/todo/src/app/component/todosList/todosList.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todosList',
+  templateUrl: './todosList.component.html',
+  styleUrls: ['./todosList.component.css']
+})
+export class TodosListComponent {
+
+}
