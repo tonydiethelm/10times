91374727b196c1e2decb7b57d40018b7239c8689
Tony Diethelm <tonydiethelm@gmail.com>
2023-05-31 14:22:41 -0700

    Adding Svelte and some angular files

--- Angular/todo/src/app/app.module.ts
+++ Angular/todo/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodosListComponent } from './component/todosList/todosList.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosListComponent
   ],
   imports: [
     BrowserModule
